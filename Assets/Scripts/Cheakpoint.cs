398550e756932638ec86f37ebaa134dd82550332
Robsome <askrobanything@gmail.com>
2019-01-26 14:33:49 -0500

    flower animations

--- Assets/Scripts/Cheakpoint.cs
+++ Assets/Scripts/Cheakpoint.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Cheakpoint : MonoBehaviour
 {
     public GameObject thePlayer;
+    private Animator flower;
+    private void Start()
+    {
+        flower = gameObject.GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D coll2D)
     {
         if (coll2D.gameObject.CompareTag("Player"))
         {
             thePlayer.GetComponent<Respawn>().currentCheakpoint = transform;
+            flower.SetBool("IsOpen", true);
         }
     }
 }
