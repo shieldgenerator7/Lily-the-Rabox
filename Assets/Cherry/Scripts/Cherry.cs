b135887141ad5cb3ee75516890d155bf2e957aa0
Robsome <askrobanything@gmail.com>
2019-01-26 00:25:29 -0500

    cherries are a thing

---
+++ Assets/Cherry/Scripts/Cherry.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cherry : MonoBehaviour
+{
+    public GameObject collectable;
+    private void OnTriggerEnter2D(Collider2D coll2D)
+    {
+        if (coll2D.gameObject.CompareTag("Player")) {
+            collectable.GetComponent<Collectables>().cherry++;
+            Destroy(gameObject);
+            }
+    }
+}
