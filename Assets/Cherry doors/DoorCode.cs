d435685e0e74a28984e3e21488958511dbdb2454
Robsome <askrobanything@gmail.com>
2019-01-26 06:07:20 -0500

    added cherry door

---
+++ Assets/Cherry doors/DoorCode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCode : MonoBehaviour
+{
+    public int cherryCount;
+    public GameObject cherryFinder;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (cherryFinder.GetComponent<Collectables>().cherry == cherryCount)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
