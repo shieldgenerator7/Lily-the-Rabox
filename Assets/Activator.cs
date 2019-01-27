efb42b4c237ff0125687b986a323ee4d5d128e0e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-01-27 16:08:40 -0500

    Added Outro Scene

---
+++ Assets/Activator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public GameObject go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            go.SetActive(true);
+        }
+    }
+}
